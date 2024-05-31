d20c6d2e9d7e96a824f9eccd763ce2201b01705c
tomoko takahashi <toutetu144@gmail.com>
2024-06-01 05:42:57 +0900

    サービスプロバイダのテスト

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,6 +8,9 @@
 | The first thing we will do is create a new Laravel application instance
 | which serves as the "glue" for all the components of Laravel, and is
 | the IoC container for the system binding all of the various parts.
+| まず最初に、新しいLaravelアプリケーションインスタンスを作成します。
+| このインスタンスは、Laravelのすべてのコンポーネントの「接着剤」として機能し、
+| システムのさまざまな部分を結びつけるIoCコンテナです。
 |
 */
 
@@ -23,6 +26,9 @@ $app = new Illuminate\Foundation\Application(
 | Next, we need to bind some important interfaces into the container so
 | we will be able to resolve them when needed. The kernels serve the
 | incoming requests to this application from both the web and CLI.
+| 次に、いくつかの重要なインターフェースをコンテナにバインドする必要があります。
+| これにより、必要に応じてそれらを解決できるようになります。
+| カーネルは、WebおよびCLIからこのアプリケーションに送信されるリクエストを処理します。
 |
 */
 
@@ -49,6 +55,9 @@ $app->singleton(
 | This script returns the application instance. The instance is given to
 | the calling script so we can separate the building of the instances
 | from the actual running of the application and sending responses.
+| このスクリプトはアプリケーションインスタンスを返します。
+| インスタンスは呼び出し元のスクリプトに渡され、
+| インスタンスの構築とアプリケーションの実行およびレスポンスの送信を分離できます。
 |
 */
 
--- config/app.php
+++ config/app.php
@@ -268,6 +268,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\SampleServiceProvider::class,
+        
 
     ],
 
