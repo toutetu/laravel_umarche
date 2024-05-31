59181807f6f8a2743f6a330f8dea7d9e58313dd5
tomoko takahashi <toutetu144@gmail.com>
2024-06-01 01:58:15 +0900

    属性バック

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -4,7 +4,10 @@
     'title' => "タイトル初期値です。"
 ])
 
-<div class="w-1/4 p-2  border-red-500 shadow-md ">
+<div {{$attributes ->merge([
+    "class" => "w-1/4 p-2  border-2 shadow-md "
+    ])}}>
+
     <div>{{ $title }}</div>
     <div>画像</div>
     <div>{{ $content }}</div>
--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -5,4 +5,5 @@
 
 <x-tests.card title="タイトル1" content="本文" :message="$message"></x-tests.card>
 <x-tests.card title="タイトル2" ></x-tests.card>
+<x-tests.card title="cssを変更したい" class="bg-red-300"></x-tests.card>
 </x-tests.app>
