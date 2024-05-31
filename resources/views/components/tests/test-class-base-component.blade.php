0649fe2d7d5eec1fdafc7eb1bc03e7f71e87243a
tomoko takahashi <toutetu144@gmail.com>
2024-06-01 02:34:14 +0900

    classcomponent

---
+++ resources/views/components/tests/test-class-base-component.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <!-- When there is no desire, all things are at peace. - Laozi -->
+    クラスベースのコンポーネント
+
+    <div>{{$classBaseMessage}}</div>
+    <div>{{$defaultMessage}}</div>
+
+</div>
