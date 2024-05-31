95dbf311cd0e9ce491dcd9ee3f57d93c67795cb3
tomoko takahashi <toutetu144@gmail.com>
2024-06-01 01:50:47 +0900

    Props

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -1,3 +1,9 @@
+@props([
+    "message" => "初期値です。",
+    'content' => "本文初期値です。",
+    'title' => "タイトル初期値です。"
+])
+
 <div class="w-1/4 p-2  border-red-500 shadow-md ">
     <div>{{ $title }}</div>
     <div>画像</div>
--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -3,6 +3,6 @@
      
      コンポーネントテスト１です。
 
-<x-tests.card title="タイトル" content="本文" :message="$message"></x-tests.card>
-
+<x-tests.card title="タイトル1" content="本文" :message="$message"></x-tests.card>
+<x-tests.card title="タイトル2" ></x-tests.card>
 </x-tests.app>
